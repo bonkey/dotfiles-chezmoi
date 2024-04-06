72e9fefbf68f362f414ab85d974c9200417ae1e1
Daniel Bauke <daniel@bauke.pl>
2024-04-06 23:10:57 +0200

    Changed dot_aliases.tmpl dot_config/Brewfile exact_bin/Brewfile exact_bin/executable_brewfile-describe.rb run_onchange_30_install_brew.tmpl

--- exact_bin/executable_brewfile-describe.rb
+++ exact_bin/executable_brewfile-describe.rb
@@ -1,23 +1,49 @@
 #!/usr/bin/env ruby
 
 require 'json'
+require 'tempfile'
 
-ARGF.each do |line|
-  if line =~ /^brew ["']([^"']+)["']$/
-    name = Regexp.last_match(1)
-    warn "Getting brew #{name}"
-    output = `brew info --json=v2 "#{name}"`
-    json = JSON.parse(output)
-    desc = json['formulae'][0]['desc']
-    puts "#{line.strip} # #{desc}"
-  elsif line =~ /^cask ["']([^"']+)["']$/
-    name = Regexp.last_match(1)
-    warn "Getting cask #{name}"
-    output = `brew info --cask --json=v2 "#{name}"`
-    json = JSON.parse(output)
-    desc = json['casks'][0]['desc']
-    puts "#{line.strip} # #{desc}"
-  else
-    puts line
+file_path = ARGV[0]
+
+def process_line(line)
+  match = line.match(/^(brew|cask) ["']([^"']+)["']$/)
+  return line unless match
+
+  type, name = match.captures
+  warn "Getting #{type} #{name}"
+  info_type = type == 'brew' ? '' : '--cask'
+  json_key = type == 'brew' ? 'formulae' : 'casks'
+
+  output = `brew info #{info_type} --json=v2 "#{name}"`
+  json = JSON.parse(output)
+  desc = json[json_key][0]['desc']
+
+  "#{line.strip} # #{desc}"
+end
+
+if file_path.nil?
+  puts "Usage: #{__FILE__} <Brewfile>"
+  exit(1)
+end
+
+unless File.exist?(file_path)
+  puts "File #{file_path} does not exist."
+  exit(2)
+end
+
+original_lines = File.readlines(file_path)
+filename = File.basename(file_path)
+temp_file = Tempfile.new(filename)
+
+begin
+  original_lines.each do |line|
+    temp_file.puts(process_line(line))
   end
+
+  temp_file.close
+
+  FileUtils.mv(temp_file.path, file_path)
+ensure
+  temp_file.close
+  temp_file.unlink
 end

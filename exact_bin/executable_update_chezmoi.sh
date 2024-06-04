cb6e1255f731e131d3d07a4fe1833e1a16be7a5f
Daniel Bauke <daniel@bauke.pl>
2024-06-04 08:22:56 +0200

    Update bin/update_chezmoi.sh

--- exact_bin/executable_update_chezmoi.sh
+++ exact_bin/executable_update_chezmoi.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env zsh -l
 
-pkill Keyboard Maestro Engine'
+read -p "Quit iTerm2? (y/n) " -n 1 -r
+[[ $REPLY =~ ^[Yy]$ ]] && pkill iTerm
+
+read -p "Quit Keyboard Maestro Engine? (y/n) " -n 1 -r
+[[ $REPLY =~ ^[Yy]$ ]] && pkill 'Keyboard Maestro Engine'
 
 chezmoi update
 

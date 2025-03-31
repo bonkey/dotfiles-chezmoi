29083dd538c56975ecd5ca5fcd8b9f4a9b334690
Daniel Bauke <dbauke@league.com>
2025-03-31 19:07:03 +0200

    Update bin/jetbrains/studio Update bin/raycast/restart-streamdeck.sh

--- exact_bin/raycast/executable_restart-streamdeck.sh
+++ exact_bin/raycast/executable_restart-streamdeck.sh
@@ -2,7 +2,7 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Restart Stream Deck
+# @raycast.title Restart Elgato
 # @raycast.mode silent
 
 # Optional parameters:
@@ -11,20 +11,27 @@
 # Documentation:
 # @raycast.author Daniel Bauke
 
+source "${HOME}/bin/_common.zsh"
+
 killall "Stream Deck"
 
-for deck_id in '0fd9:0084' '0fd9:0080' ; do
-    echo "Searching for Stream Deck at ${deck_id}..."
-    SD_HUB=$(uhubctl --search "$deck_id" | head -2  | grep "Current status for hub" | sed -E 's/Current status for hub ([0-9.-]+).*/\1/g')
-    SD_PORT=$(uhubctl --search "$deck_id" | head -2 | grep -A 1 "Current status for hub" | grep "Port" | sed -E 's/.*Port ([0-9]+).*/\1/g')
+device_ids=($(uhubctl|grep "Elgato" | grep -o '\[.*\]' | grep -o '0fd9:[0-9a-f]\+'))
 
-    if [ -n "$SD_HUB" ] && [ -n "$SD_PORT" ]; then
-        echo "✅ Found Stream Deck on hub $SD_HUB port $SD_PORT. Restarting..."
-        uhubctl -a cycle -d 2 -l $SD_HUB -p $SD_PORT
-    else
-        echo "❌ Stream Deck not found"
-    fi
-done
+if [ ${#device_ids[@]} -eq 0 ]; then
+    echo "❌ No Elgato devices found"
+else
+    for device_id in "${device_ids[@]}"; do
+        echo "Searching for Stream Deck at ${device_id}..."
+        SD_HUB=$(uhubctl --exact --nodesc --search "$device_id" | head -2  | grep "Current status for hub" | sed -E 's/Current status for hub ([0-9.-]+).*/\1/g')
+        SD_PORT=$(uhubctl --exact --nodesc --search "$device_id" | head -2 | grep -A 1 "Current status for hub" | grep "Port" | sed -E 's/.*Port ([0-9]+).*/\1/g')
 
-sleep 5
+        if [ -n "$SD_HUB" ] && [ -n "$SD_PORT" ]; then
+            echo "✅ Found a device on hub $SD_HUB port $SD_PORT. Restarting..."
+            _exec "uhubctl --exact --nodesc --action cycle --delay 2 --location $SD_HUB --port $SD_PORT"
+        else
+            echo "❌ Device ${device_id} not found"
+        fi
+    done
+fi
+
 open -a "Elgato Stream Deck"

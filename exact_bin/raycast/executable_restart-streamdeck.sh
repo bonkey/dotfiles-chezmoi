391b54d7e868903f4a69021a9be7d558563000e3
Daniel Bauke <dbauke@league.com>
2025-03-31 14:19:43 +0200

    Update bin/raycast/restart-streamdeck.sh

--- exact_bin/raycast/executable_restart-streamdeck.sh
+++ exact_bin/raycast/executable_restart-streamdeck.sh
@@ -12,7 +12,19 @@
 # @raycast.author Daniel Bauke
 
 killall "Stream Deck"
-uhubctl -a cycle -d 2 -l 0-2.4.1 -p 2
-uhubctl -a cycle -d 2 -l 0-1.1.2 -p 1
+
+for deck_id in '0fd9:0084' '0fd9:0080' ; do
+    echo "Searching for Stream Deck at ${deck_id}..."
+    SD_HUB=$(uhubctl --search "$deck_id" | head -2  | grep "Current status for hub" | sed -E 's/Current status for hub ([0-9.-]+).*/\1/g')
+    SD_PORT=$(uhubctl --search "$deck_id" | head -2 | grep -A 1 "Current status for hub" | grep "Port" | sed -E 's/.*Port ([0-9]+).*/\1/g')
+
+    if [ -n "$SD_HUB" ] && [ -n "$SD_PORT" ]; then
+        echo "✅ Found Stream Deck on hub $SD_HUB port $SD_PORT. Restarting..."
+        uhubctl -a cycle -d 2 -l $SD_HUB -p $SD_PORT
+    else
+        echo "❌ Stream Deck not found"
+    fi
+done
+
 sleep 5
 open -a "Elgato Stream Deck"

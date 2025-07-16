3ca264bec84d09f733fb42d53f4d0ff7f72e7fd4
Daniel Bauke <dbauke@league.com>
2025-07-16 16:22:26 +0200

    Update .aliases Update bin/simulator_populator.rb Update bin/update_chezmoi.sh Update bin/jetbrains/studio Update Library/Developer/Xcode/UserData/FontAndColorThemes/Customized Default (Light).xccolortheme Update Library/Developer/Xcode/UserData/KeyBindings/Default.idekeybindings

--- exact_bin/executable_update_chezmoi.sh
+++ exact_bin/executable_update_chezmoi.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env zsh -l
 
+function read_key {
+    local oldstty=$(stty -g)
+    stty -icanon -echo min 1 time 0
+    local char=$(dd bs=1 count=1 2>/dev/null)
+    stty $oldstty
+    echo $char
+}
+
 function open_applications {
     open -a 'Keyboard Maestro Engine'
     open -a iTerm
@@ -11,6 +19,13 @@ function countdown() {
         sleep 1
     done
 }
+
+if [[ "$__CFBundleIdentifier" != "com.googlecode.iterm2" ]]; then
+    echo -n "Quit iTerm2? (y/n) "
+    response=$(read_key)
+    echo
+    [[ $response == "y" ]] && pkill iTerm
+fi
 
 pkill 'Keyboard Maestro Engine'
 

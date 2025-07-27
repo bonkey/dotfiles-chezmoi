86dd33f2fb337829261a1dc375cef1372a5b0b1f
Daniel Bauke <daniel@bauke.pl>
2025-07-27 15:01:24 +0200

    Add bin/simulator_populator.rb Add bin/litra/auto-on/uninstall.sh Add bin/raycast/turn-off-ensemble.sh Remove bin/simulator_populator.rb Change attributes of bin/_common.zsh Change attributes of bin/appstat Change attributes of bin/benchmark-urls.sh Change attributes of bin/brewfile-describe.rb Change attributes of bin/cartool Change attributes of bin/codeowners-validator Change attributes of bin/connect_macmini.sh Change attributes of bin/convert_csv.rb Change attributes of bin/fix-xcode-for-latest-macos.sh Change attributes of bin/flash-glove80.sh Change attributes of bin/get-links.rb Change attributes of bin/git-loc Change attributes of bin/gyb Change attributes of bin/hidapitester Change attributes of bin/mount-ssd.sh Change attributes of bin/nsscreencast-download.rb Change attributes of bin/organize_ebooks.py Change attributes of bin/razer-led-off.sh Change attributes of bin/restart-eth.sh Change attributes of bin/srt_delay.rb Change attributes of bin/to-srt.py Change attributes of bin/update_chezmoi.sh Change attributes of bin/vimer Change attributes of bin/zip-account-papers.sh Change attributes of bin/gyb.py Change attributes of bin/jetbrains/studio Change attributes of bin/litra/_litra.zsh Change attributes of bin/litra/auto-on/install.sh Change attributes of bin/litra/auto-on/litra-auto-on.plist Change attributes of bin/litra/auto-on/litra-auto-on.sh Change attributes of bin/litra/auto-on/symlink_uninstall.sh Change attributes of bin/litra/_litra-run.sh Change attributes of bin/litra/litra-level1.sh Change attributes of bin/litra/litra-level2.sh Change attributes of bin/litra/litra-level3.sh Change attributes of bin/litra/litra-level4.sh Change attributes of bin/litra/litra-level5.sh Change attributes of bin/litra/litra-off.sh Change attributes of bin/litra/litra-on.sh Change attributes of bin/litra/litra-temp1.sh Change attributes of bin/litra/litra-temp2.sh Change attributes of bin/litra/litra-temp3.sh Change attributes of bin/litra/litra-temp4.sh Change attributes of bin/litra/litra-temp5.sh Change attributes of bin/pages2PDF.app/Contents/Info.plist Change attributes of bin/pages2PDF.app/Contents/MacOS/droplet Change attributes of bin/pages2PDF.app/Contents/PkgInfo Change attributes of bin/pages2PDF.app/Contents/Resources/Scripts/main.scpt Change attributes of bin/pages2PDF.app/Contents/Resources/description.rtfd/TXT.rtf Change attributes of bin/pages2PDF.app/Contents/Resources/droplet.icns Change attributes of bin/pages2PDF.app/Contents/Resources/droplet.rsrc Change attributes of bin/pages2PDF.app/Contents/_CodeSignature/CodeResources Change attributes of bin/raycast/restart-streamdeck.sh Change attributes of bin/raycast/switch-litra-glow.sh Change attributes of bin/raycast/switch-resolution.sh Change attributes of bin/update_chezmoi.terminal

---
+++ bin/raycast/executable_turn-off-ensemble.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Restart Universal Control
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ⌨️
+
+# Documentation:
+# @raycast.author Daniel Bauke
+
+sudo launchctl stop com.apple.ensemble

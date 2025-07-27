dcf8667555791e147eae659ea67412725ee30f47
Daniel Bauke <daniel@bauke.pl>
2025-07-27 15:01:24 +0200

    Add bin/simulator_populator.rb Add bin/litra/auto-on/uninstall.sh Add bin/raycast/turn-off-ensemble.sh Remove bin/simulator_populator.rb Change attributes of bin/_common.zsh Change attributes of bin/appstat Change attributes of bin/benchmark-urls.sh Change attributes of bin/brewfile-describe.rb Change attributes of bin/cartool Change attributes of bin/codeowners-validator Change attributes of bin/connect_macmini.sh Change attributes of bin/convert_csv.rb Change attributes of bin/fix-xcode-for-latest-macos.sh Change attributes of bin/flash-glove80.sh Change attributes of bin/get-links.rb Change attributes of bin/git-loc Change attributes of bin/gyb Change attributes of bin/hidapitester Change attributes of bin/mount-ssd.sh Change attributes of bin/nsscreencast-download.rb Change attributes of bin/organize_ebooks.py Change attributes of bin/razer-led-off.sh Change attributes of bin/restart-eth.sh Change attributes of bin/srt_delay.rb Change attributes of bin/to-srt.py Change attributes of bin/update_chezmoi.sh Change attributes of bin/vimer Change attributes of bin/zip-account-papers.sh Change attributes of bin/gyb.py Change attributes of bin/jetbrains/studio Change attributes of bin/litra/_litra.zsh Change attributes of bin/litra/auto-on/install.sh Change attributes of bin/litra/auto-on/litra-auto-on.plist Change attributes of bin/litra/auto-on/litra-auto-on.sh Change attributes of bin/litra/auto-on/symlink_uninstall.sh Change attributes of bin/litra/_litra-run.sh Change attributes of bin/litra/litra-level1.sh Change attributes of bin/litra/litra-level2.sh Change attributes of bin/litra/litra-level3.sh Change attributes of bin/litra/litra-level4.sh Change attributes of bin/litra/litra-level5.sh Change attributes of bin/litra/litra-off.sh Change attributes of bin/litra/litra-on.sh Change attributes of bin/litra/litra-temp1.sh Change attributes of bin/litra/litra-temp2.sh Change attributes of bin/litra/litra-temp3.sh Change attributes of bin/litra/litra-temp4.sh Change attributes of bin/litra/litra-temp5.sh Change attributes of bin/pages2PDF.app/Contents/Info.plist Change attributes of bin/pages2PDF.app/Contents/MacOS/droplet Change attributes of bin/pages2PDF.app/Contents/PkgInfo Change attributes of bin/pages2PDF.app/Contents/Resources/Scripts/main.scpt Change attributes of bin/pages2PDF.app/Contents/Resources/description.rtfd/TXT.rtf Change attributes of bin/pages2PDF.app/Contents/Resources/droplet.icns Change attributes of bin/pages2PDF.app/Contents/Resources/droplet.rsrc Change attributes of bin/pages2PDF.app/Contents/_CodeSignature/CodeResources Change attributes of bin/raycast/restart-streamdeck.sh Change attributes of bin/raycast/switch-litra-glow.sh Change attributes of bin/raycast/switch-resolution.sh Change attributes of bin/update_chezmoi.terminal

---
+++ bin/executable_simulator_populator.rb
@@ -0,0 +1,147 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'json'
+require 'rainbow'
+require 'English'
+
+# xcrun simctl list -j devicetypes | jq '.devicetypes[].name'|sort
+devices_to_create = [
+  'iPad Pro 11-inch (M4)',
+  'iPad Pro 13-inch (M4)',
+  'iPhone 16 Pro Max',
+  'iPhone 16 Pro',
+  'iPhone 16',
+  'iPhone SE (3rd generation)'
+]
+
+# xcrun simctl list -j runtimes | jq '.runtimes[].name'
+runtimes_to_use = [
+  'iOS 17.5',
+  'iOS 18.2',
+  'iOS 18.3',
+  'iOS 18.4',
+  'iOS 18.5',
+  'iOS 26.0'
+]
+
+default_sim_device = 'iPhone 15 Pro'
+default_sim_runtime = 'iOS 17.5'
+
+class SimulatorPopulator
+  def initialize(
+    devices_to_create:, runtimes_to_use:,
+    default_device:, default_runtime:
+  )
+    @devices_to_create = devices_to_create
+    @runtimes_to_use = runtimes_to_use
+    @default_device = default_device
+    @default_runtime = default_runtime
+
+    @device_types = JSON.parse `xcrun simctl list -j devicetypes`
+    @runtimes = JSON.parse `xcrun simctl list -j runtimes`
+    @devices = JSON.parse `xcrun simctl list -j devices`
+    @available_runtimes = @runtimes['runtimes'].select do |runtime|
+      runtime['availability'] == '(available)' || runtime['isAvailable'] == true
+    end
+  end
+
+  def remove_all
+    puts 'Removing all existing simulators...'
+    @devices['devices'].each do |_, runtime_devices|
+      runtime_devices.each do |device|
+        puts 'Removing: ' +
+             Rainbow("#{device['name']} (#{device['udid']})").color(:red).bright
+        `xcrun simctl delete #{device['udid']}`
+      end
+    end
+  end
+
+  def create
+    puts 'Creating all simulator variants...'
+
+    @available_runtimes.each do |runtime|
+      next unless @runtimes_to_use.include?(runtime['name'])
+
+      puts Rainbow("## #{runtime['name']}").color(:blue).bright
+      @device_types['devicetypes'].each do |device_type|
+        next unless @devices_to_create.include?(device_type['name'])
+
+        create_device(device_type: device_type['identifier'],
+                      runtime: runtime['name'],
+                      name: "#{device_type['name']} (#{runtime['name']})")
+      end
+    end
+  end
+
+  def create_default_device
+    puts "Creating default device #{Rainbow(@default_device).color(:green).bright} on #{Rainbow(@default_runtime).color(:green).bright}"
+    create_device(device_type: @default_device, runtime: @default_runtime)
+  end
+
+  private
+
+  def find_runtime(name:)
+    runtime = @available_runtimes
+              .select { |runtime| runtime['name'] == name }
+              .first
+
+    return if runtime.nil?
+
+    runtime['identifier']
+  end
+
+  def create_device(device_type:, runtime:, name: nil)
+    name ||= device_type
+    runtime_id = find_runtime(name: runtime)
+    if runtime_id.nil?
+      puts "Runtime #{Rainbow(runtime).color(:red).bright} not found"
+      return
+    end
+    args = ["'#{name}'", "'#{device_type}'", "'#{runtime_id}'"].join ' '
+
+    `xcrun simctl create #{args} 2> /dev/null`
+    puts "Created #{Rainbow(name).color(:green).bright}" if $CHILD_STATUS.success?
+  end
+end
+
+options = {
+  'remove-existing' => true,
+  'create-all-variants' => true,
+  'create-default-variant' => true,
+  'verbose' => true
+}
+
+option_parser = OptionParser.new do |opts|
+  opts.on '-r', '--[no-]remove-existing', 'Remove all existing simulators' do |v|
+    options['remove-existing'] = v
+  end
+  opts.on '-c', '--[no-]create-all-variants', 'Create new simulators' do |v|
+    options['create-all-variants'] = v
+  end
+  opts.on '-d', '--[no-]create-default-variant', "Create a default simulator (#{default_sim_device} on #{default_sim_runtime})" do |v|
+    options['create-default-variant'] = v
+  end
+  opts.on '-v', '--[no-]verbose', 'Make the operation more talkative (not really, not implemented yet)' do |v|
+    options['verbose'] = v
+  end
+  opts.on '-h', '--help', 'This help'
+end
+
+option_parser.parse!
+
+if options[:help]
+  puts option_parser
+  exit
+end
+
+populator = SimulatorPopulator.new(
+  devices_to_create: devices_to_create,
+  runtimes_to_use: runtimes_to_use,
+  default_device: default_sim_device,
+  default_runtime: default_sim_runtime
+)
+
+populator.remove_all if options['remove-existing']
+populator.create if options['create-all-variants']
+populator.create_default_device if options['create-default-variant']

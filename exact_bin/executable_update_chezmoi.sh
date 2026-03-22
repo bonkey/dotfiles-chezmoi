ba8bbf01cf9c094884a2787fa290619198bd48fc
Daniel Bauke <daniel@bauke.pl>
2026-03-22 20:10:24 +0100

    Update bin/update_chezmoi.sh

--- exact_bin/executable_update_chezmoi.sh
+++ exact_bin/executable_update_chezmoi.sh
@@ -1,17 +1,45 @@
 #!/usr/bin/env zsh -l
 
-function open_applications {
+stop_apps() {
+    pkill 'Keyboard Maestro Engine'
+}
+
+open_apps() {
     open -a 'Keyboard Maestro Engine'
     open -a iTerm
 }
 
-function countdown() {
+countdown() {
+    figlet 'chezmoi updated!'
     for i in {5..1}; do
         figlet $i
         sleep 1
     done
 }
 
-pkill 'Keyboard Maestro Engine'
+notify() {
+    local msg=$1
+    if command -v noti &>/dev/null; then
+        noti -m "$msg" -t 'chezmoi update'
+    fi
+}
 
-chezmoi update && open_applications && countdown
+handle_result() {
+    local exit_code=$1
+    if [ $exit_code -ne 0 ]; then
+        notify 'failed'
+        echo "Chezmoi update failed. Press any key to exit..."
+        read -k1
+    else
+        notify 'successful'
+        countdown
+    fi
+}
+
+stop_apps
+
+chezmoi update
+CHEZMOI_EXIT_CODE=$?
+
+open_apps
+handle_result $CHEZMOI_EXIT_CODE

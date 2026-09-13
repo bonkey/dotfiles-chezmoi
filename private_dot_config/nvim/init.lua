310511dafdc006b7ae1a4a859e6d0b902c8bf564
Daniel Bauke <daniel@bauke.pl>
2026-09-13 14:36:54 +0200

    Add .config/nvim/init.lua Update 115_update_herdr

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,4 @@
+-- Publish the current file name as the terminal title, so herdr-auto-title can
+-- name the tab "nvim › <file>" instead of a bare "nvim".
+vim.opt.title = true
+vim.opt.titlestring = "%t"
